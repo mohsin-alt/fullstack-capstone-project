ad3231048698b5a5be4bfa5ac687e58c9943c52d
mohsin-alt <blue1045@gmail.com>
2025-04-10 09:28:24 -0400

    done with Coding

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import {urlConfig} from '../../config';
+
 import { useAppContext } from '../../context/AuthContext';
 export default function Navbar() {
     const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
